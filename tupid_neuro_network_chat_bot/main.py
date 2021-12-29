8a427a5d4bc0d52a495329a6651d799e0fea871c
Vladimir-Snegur <51735672+Vladimir-Snegur@users.noreply.github.com>
2021-12-29 23:36:18 +0300

    Add files via upload

---
+++ tupid_neuro_network_chat_bot/main.py
@@ -0,0 +1,17 @@
+
+print ("Как тебя зовут?")
+
+name = input()
+
+print("Привет, ", name+"!")
+
+print("Как у тебя дела?")
+
+users_request = input()
+
+print("Это ", users_request.lower())
+
+print("Ну что ж, рад был с тобой, поболтать! Заходи еще, если что.")
+
+
+
